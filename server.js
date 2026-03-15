cb5ef861fbbe78bdb43b07583d5fceceed885f56
liordesign7-code <liordesign7@gmail.com>
2026-03-15 16:29:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const twilioRoutes = require('./routes/twilio')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// Twilio webhook
+app.use('/', twilioRoutes)
 
 app.get('/', (req, res) => {
   res.json({ ok: true, message: 'Lior Design CRM API עובד' })
