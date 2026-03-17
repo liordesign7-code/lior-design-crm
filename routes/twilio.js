1b9e9411c0a61d6eba25b951d45d48acaa09b23f
liordesign7-code <liordesign7@gmail.com>
2026-03-17 17:52:24 +0200

    Update twilio.js

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// בדיקה שהשרת עובד
+// בדיקה
 router.get('/test', (req, res) => {
   res.json({ ok: true, msg: 'twilio route works' });
 });
 
-module.exports = router;
+// קבלת הודעות וואטסאפ
+router.post('/incoming-wa', (req, res) => {
+  res.set('Content-Type', 'text/xml');
+  res.send('<Response></Response>');
+
+  const from = req.body?.From || '';
+  const body = req.body?.Body || '';
+
+  console.log('📩
