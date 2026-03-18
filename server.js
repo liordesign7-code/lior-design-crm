1a3ce339951d4d1f916a7d160c4074aabee4bd28
liordesign7-code <liordesign7@gmail.com>
2026-03-19 01:47:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('/etc/secrets/firebase.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 require('dotenv').config();
 const express    = require('express');
 const cors       = require('cors');
